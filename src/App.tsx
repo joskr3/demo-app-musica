b11759d7c2f8fe219eeeb8bbefe3f3d1f52c9591
Josue Patricio <josretamozovar@gmail.com>
2024-09-04 19:32:40 -0500

    setup inicial, configuracion del proyecto react, usando vite, con typescript y taildwindcss con shadcn

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Button>Click me</Button>
+    </>
+
+  )
+}
+
+export default App
