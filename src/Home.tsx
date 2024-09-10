8cc2d4b1876ea5957084775da26e10ecc29f4883
Josue Patricio <josretamozovar@gmail.com>
2024-09-09 19:20:50 -0500

    agregar menu responsivo

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,14 @@
 import Main from "./components/custom/Main"
 import NavBar from "./components/custom/NavBar"
 import SideBar from "./components/custom/SideBar"
+import SideBarDesktop from "./components/custom/SideBarDesktop"
 
 const Home = () => {
   return (
     <div className="flex flex-col h-dvh w-dvw">
       <NavBar />
       <div className="flex flex-1 w-full">
+        <SideBarDesktop/>
         <SideBar />
         <Main />
       </div>
---
+++ src/components/custom/SideBarDesktop.tsx
@@ -0,0 +1,12 @@
+
+const SideBarDesktop = () => {
+  return (
+    <aside className="hidden md:flex flex-col justify-between p-2 border border-solid border-zinc-400 ">
+      <p>1</p>
+      <p>2</p>
+      <p>3</p>
+    </aside>
+  )
+}
+
+export default SideBarDesktop
