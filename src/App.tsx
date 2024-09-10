e8799a7e4a921cc591037ee8bc2892f9a71ed167
Josue Patricio <josretamozovar@gmail.com>
2024-09-09 20:12:00 -0500

    fix: muestra el contenido correcto en la version desktop de sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Route, Switch } from "wouter"
 import Home from "./Home"
 import NotFound from "./NotFound"
+import LoginPage from "./components/authentication-04"
 
 const App = () => {
   return (
     <Switch>
       <Route path="/" component={Home} />
+      <Route path="/login" component={LoginPage} />
+      
       {/* Default route in a switch */}
       <Route component={NotFound} />
     </Switch>
--- src/components/custom/SideBarDesktop.tsx
+++ src/components/custom/SideBarDesktop.tsx
@@ -1,10 +1,9 @@
+import ContentSideBar from "./ContentSideBar"
 
 const SideBarDesktop = () => {
   return (
-    <aside className="hidden md:flex flex-col justify-between p-2 border border-solid border-zinc-400 ">
-      <p>1</p>
-      <p>2</p>
-      <p>3</p>
+    <aside className="hidden md:flex flex-col gap-12 p-2 ">
+      <ContentSideBar />
     </aside>
   )
 }
