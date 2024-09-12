27acc12933c3c1c650ba9800a84a1c69b15ed062
Josue Patricio <josretamozovar@gmail.com>
2024-09-11 19:52:50 -0500

    fix: cambios menores

--- src/components/custom/SectionCarousel.tsx
+++ src/components/custom/SectionCarousel.tsx
@@ -1,5 +1,7 @@
 import type { CardMusicProps } from "@/interfaces/ICardMusic"
 import CardMusic from "./CardMusic"
+import { Separator } from "../ui/separator"
+
 
 interface Props {
   titulo: string
@@ -10,15 +12,17 @@ interface Props {
 const SectionCarousel = ({ titulo, descripcion, cards }: Props) => {
   return (
     <div className="flex flex-col gap-4">
-      <h1>{titulo}</h1>
-      <p>{descripcion}</p>
-      <div className="flex gap-10 overflow-x-auto  max-w-full">
+      <h1 className="text-2xl md:text-3xl font-extralight">{titulo}</h1>
+      <p className="font-extralight">{descripcion}</p>
+      <Separator/>
+      <div className="flex gap-2 overflow-x-auto  max-w-full">
         {
           cards.map((card, index) =>
             <CardMusic key={index} url={card.url} titulo={card.titulo} subtitulo={card.subtitulo} />
           )
         }
       </div>
+      <Separator />
     </div>
   )
 }
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-xl border bg-card text-card-foreground shadow  min-w-full  md:min-w-fit",
+      "rounded-xl  bg-card text-card-foreground  min-w-full  md:min-w-fit",
       className
     )}
     {...props}
